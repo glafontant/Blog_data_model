387facb80397c3646f1cebcca1c8a50a7619422c
Gabriel Lafontant <glafonta@gmail.com>
2013-09-11 17:44:02 -0400

    created author model

---
+++ spec/factories/authors.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :author do
+    first_name "MyString"
+    las_name "MyString"
+    email "MyString"
+  end
+end
