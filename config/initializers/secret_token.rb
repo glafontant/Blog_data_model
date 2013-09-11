d483c87ef04bab2e71ee6002cf80da71c5ac64c5
Gabriel Lafontant <glafonta@gmail.com>
2013-09-11 17:24:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoolBlog::Application.config.secret_key_base = 'f8a5600e9d7eb3a79b115b58e2bd377e7ea3aa73a4ce3a686d98484c7099e246b934db242ea4c7cd382e73cb3e215f7c7451775042ffa6863799c5e482f487b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoolBlog::Application.config.session_store :cookie_store, key: '_cool_blog_session'
